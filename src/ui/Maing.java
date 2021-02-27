1266e64a3c62de7f45554324c392e793d8a7d9c6
Cristian David Del Castillo Garcia <rocosa2003@gmail.com>
2021-02-27 01:40:51 -0500

    mi primer git

---
+++ src/ui/Maing.java
@@ -0,0 +1,25 @@
+package ui;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Maing {
+	public final static String separate = " "; 
+	public static void main(String[] args ) throws IOException {
+		String out = "";
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+		
+		String txin = br.readLine();
+		int line = Integer.parseInt(txin);
+		
+		
+	}
+	private static String bubbleSort() {
+	}
+
+
+}
